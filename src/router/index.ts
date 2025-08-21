ea7949559b0449243467f4c1aff9198e13f212df
Diego Reyes <diegorele13@gmail.com>
2025-08-21 16:09:08 -0500

    feat(router): add invitation route and remove home route
    Replace home route with new invitation route to support the new invitation feature

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
+import InvitationView from '@/views/InvitationView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/invitation',
+      name: 'invitation',
+      component: InvitationView,
     },
   ],
 })
