ec756891921697699d3b43095055662b421b6271
Diego Reyes <diegorele13@gmail.com>
2025-08-21 15:07:38 -0500

    fix: update scss import path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use "@/styles/colorVariables.module.scss" as *;`,
+        additionalData: `@use "@/styles/index.scss" as *;`,
       },
     },
   },
