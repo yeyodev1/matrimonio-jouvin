4d54948d4263409b5c5dd9cbcb1982b7d037cce7
Diego Reyes <diegorele13@gmail.com>
2025-08-24 15:27:07 -0500

    feat(router): add create invitation route and view component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import InvitationView from '@/views/InvitationView.vue'
+import CreateInvitationView from '@/views/CreateInvitationView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/invitation',
       name: 'invitation',
       component: InvitationView,
+    },
+    {
+      path: '/create-invitation',
+      name: 'create-invitation',
+      component: CreateInvitationView,
     },
   ],
 })
